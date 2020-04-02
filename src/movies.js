6d3583cc6191c7caab053361916edbbfc49b21cf
Cule219 <culaficstefan@gmail.com>
2020-04-02 18:12:57 -0400

    iteration-3

--- src/movies.js
+++ src/movies.js
@@ -1,26 +1,26 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
-const orderByYear = (arr) => {
+const orderByYear = arr => {
   // concat() method combines two arrays
-  let sortedArr = [...arr]  // ... spread operator - we make a copy of the arr https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+  let sortedArr = [...arr]; // ... spread operator - we make a copy of the arr https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 
-  return sortedArr.sort((a,b) => {
-    if(a.year === b.year){
-      return a.title.localeCompare(b.title) // localeCompare - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
+  return sortedArr.sort((a, b) => {
+    if (a.year === b.year) {
+      return a.title.localeCompare(b.title); // localeCompare - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
     } else {
-      return a.year - b.year
+      return a.year - b.year;
     }
-  })
+  });
 
   // return sortedArr.sort((a,b) => {
-  //   if(a.year === b.year){      // if the year is the same then order by title 
+  //   if(a.year === b.year){      // if the year is the same then order by title
   //     if(a.title < b.title){
   //       return -1     // the position of the values do not change
   //     } else if(a.title > b.title){
-  //       return 1      // the position of the values are swapped 
+  //       return 1      // the position of the values are swapped
   //     } else {
-  //       return 0      // the values are the same, no changes 
+  //       return 0      // the values are the same, no changes
   //     }
   //   } else {
   //     return a.year - b.year
@@ -28,29 +28,56 @@ const orderByYear = (arr) => {
   // })
 
   // return sortedArr
-}
+};
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
-const howManyMovies = (movies) => {
-    // let totalCount = 0;
-    // for(let i=0; i<movies.length; i++){
-    //     let bothConditions = movies[i].genre.includes('Drama') && movies[i].director.includes('Steven Spielberg')
-    //     if(bothConditions){
-    //         totalCount++
-    //     }
-    // }
-    // return totalCount;
+const howManyMovies = movies => {
+  // let totalCount = 0;
+  // for(let i=0; i<movies.length; i++){
+  //     let bothConditions = movies[i].genre.includes('Drama') && movies[i].director.includes('Steven Spielberg')
+  //     if(bothConditions){
+  //         totalCount++
+  //     }
+  // }
+  // return totalCount;
 
-    let filteredMovies = movies.filter(obj => {
-        if(obj.director === "Steven Spielberg" && obj.genre.includes('Drama')){
-            return obj
-        }
-    })
-    return filteredMovies.length
-}
-
+  let filteredMovies = movies.filter(obj => {
+    if (obj.director === "Steven Spielberg" && obj.genre.includes("Drama")) {
+      return obj;
+    }
+  });
+  return filteredMovies.length;
+};
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(arr) {
+  // map, filter, reduce -> make a copy of the array
+  // sort -> mutates the original array
+  let copyOfTheArray = [...arr];
+
+  copyOfTheArray.sort(function(a, b) {
+    return a.title.localeCompare(b.title);
+  }); // sort the entire 250 movies
+  let firstTwenty = copyOfTheArray.slice(0, 20); // get the first 20 movies
+  let firstTwentyTitles = firstTwenty.map(movie => {
+    return movie.title;
+  }); // get the array of first 20 titles
+  return firstTwentyTitles; // return the array of titles
+
+  // return [...arr]
+  //   .sort(function(a, b) {
+  //     return a.title.localeCompare(b.title);
+  //   })
+  //   .slice(0, 20)
+  //   .map(movie => {
+  //     return movie.title;
+  //   });
+
+  // return [...arr]
+  //   .sort((a, b) => a.title.localeCompare(b.title))
+  //   .slice(0, 20)
+  //   .map(movie => movie.title);
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
